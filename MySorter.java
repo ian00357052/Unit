521632fca336096862e4186528a8bf6d7597b35b
ian00357052 <ian00357052@gmail.com>
2016-12-26 21:32:44 +0800

    修改

--- MySorter.java
+++ MySorter.java
@@ -4,16 +4,25 @@ public class MySorter
 {
 	public void sort (double[] d)
 	{
-		for (int i = 0; i < d.length - 1; i++)
+		boolean tmp = true;
+		for (int i = 0; i < d.length; i++)
 		{
-			for (int j = 0; j < d.length - i - 1; j++)
+			if(d[i]<0) tmp = false;
+		}
+		if(tmp){
+			for (int i = 0; i < d.length - 1; i++)
 			{
-				if (d[j] > d[j + 1])
+				for (int j = 0; j < d.length - i - 1; j++)
 				{
-					swap(d, j, j + 1);
+					if (d[j] > d[j + 1])
+					{
+						swap(d, j, j + 1);
+					}
 				}
 			}
 		}
+		
+		
 	}
 
 	public void swap (double d[], int k, int s)
